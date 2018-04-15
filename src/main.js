04caaa0769212cb78f3488069a577a7d5265221d
huafeihuao0 <yx16395175@163.com>
2018-04-15 17:42:52 +0800

    重构模块化入口main.js

--- src/main.js
+++ src/main.js
@@ -1,45 +1,76 @@
 import Vue from 'vue';
-import store from './vuex/store';
+import store from './vuex/store'; //
 import VueRouter from 'vue-router';
 import routes from './routers'; // 引入路由配置
 import vueResource from 'vue-resource';
-import infiniteScroll from 'vue-infinite-scroll';  // 引入滑动模块
-import VueLazyload from 'vue-lazyload';  // 引入图片懒加载模块
-import {loadFromlLocal} from './common/js/store';  // 公共方法：本地缓存
+import infiniteScroll from 'vue-infinite-scroll'; // 引入滑动模块
+import VueLazyload from 'vue-lazyload'; // 引入图片懒加载模块
+import {loadFromlLocal} from './common/js/store'; // 公共方法：本地缓存
 import 'common/css/index.styl'; // 引入公共样式
 
-// 注册组件
-Vue.use(infiniteScroll);
-Vue.use(VueRouter);
-Vue.use(vueResource);
-
-// error，loading是图片路径, 用require引入
-Vue.use(VueLazyload, {
-    error: require('./assets/404.png'),
-    loading: require('./assets/loading.gif'),
-    attempt: 1
-  }
-);
-const router = new VueRouter({
-  'linkActiveClass': 'active',
-  routes // （缩写）相当于 routes: routes
-});
-/**
- * 创建和挂载根实例。
- * 记得要通过 router 配置参数注入路由，
- * 从而让整个应用都有路由功能
- */
-const routerApp = new Vue({
-  store,
-  router
-}).$mount('#app');
+var AppRoot;
+(function init()
+{
+  //使用插件
+  useMids();
+  //初始化并挂载应用根组件
+  const {router, routerApp} = initAppRoot();
+  //初始化welcome页面的数据集
+  loadWelcomePage(router);
+  AppRoot = routerApp;
+})()
 
 /**
- * loadFromlLocal()是读取本地缓存数据，具体common/js/store.js 查看
- *
- *
- */
-if (!loadFromlLocal('gank', 'wecome', false)) {
-  router.push('/wecome');
+ * 使用插件
+ * */
+function useMids()
+{
+
+  Vue.use(infiniteScroll); //无限滚动插件
+  Vue.use(VueRouter); //路由插件
+  Vue.use(vueResource); //http插件
+
+  //使用懒加载插件，图片未加载完成的时候占位
+  const lazyOpts =//
+    {
+      error: require('./assets/404.png'),
+      loading: require('./assets/loading.gif'),
+      attempt: 1
+    }
+  Vue.use(VueLazyload, lazyOpts);
 }
-export default routerApp;
+
+/**
+ * 初始化并挂载应用根组件
+ *
+ * */
+function initAppRoot()
+{
+  //实例化路由
+  const router = new VueRouter({
+    'linkActiveClass': 'active',
+    routes
+  });
+
+  //挂载app，帮绑定store和路由
+  const routerApp = new Vue({
+    store,  //store
+    router //路由
+  }).$mount('#app');
+
+  return {router, routerApp};
+}
+
+
+/**
+ * 初始化welcome页面的数据集
+ * */
+function loadWelcomePage(router)
+{
+  if (!loadFromlLocal('gank', 'wecome', false))
+  {
+    router.push('/wecome');
+  }
+}
+
+export default AppRoot;
